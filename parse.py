120203879f2a879e2b3cab66b55aa3584bf73c65
RasmussBrodin <rasmuss.brodin@gmail.com>
2024-04-29 13:10:08 +0200

    kaos 2

--- parse.py
+++ parse.py
@@ -1,11 +1,13 @@
 import re
+import openpyxl
 from bs4 import BeautifulSoup
-from print import db, Medicine, Print_text, app
+from print import db, Medicine, Print_text, Region, app
 app.app_context().push()
 
-## Remove all old data from the Medicine table
+## Remove all old data
 Medicine.query.delete()
 Print_text.query.delete()
+Region.query.delete()
 
 # Load the HTML content
 file_path = 'Etiketter_ny.htm'
@@ -55,7 +57,36 @@ for h1 in soup.find_all('h1'):
             print_text = Print_text(eped_id=bracket_content, print_name=print_name1, print_text=zpl_code)
             db.session.add(print_text)  # Add the Print_text instance to the session
 
-# Commit the changes to the database
+# Load the Excel workbook
+workbook = openpyxl.load_workbook('regions.xlsx')
+
+# Select the active worksheet (you can also specify a particular sheet by name)
+sheet = workbook.active
+
+# Iterate through rows and columns
+for row in sheet.iter_rows(values_only=True):
+    eped_id = row[0]
+    region_name = row[2]
+
+    # Check if the region is already added to the database
+    region = Region.query.filter_by(region=region_name).first()
+
+    if not region and region_name != "Alla instruktioner":
+        # Create a new Region instance
+        region = Region(region=region_name)
+        db.session.add(region)  # Add the Region instance to the session
+
+    # Retrieve the medicine based on eped_id
+    medicine = Medicine.query.filter_by(eped_id=eped_id).first()
+
+    if medicine and region:
+        # Add the medicine to the region's list of medicines
+        region.medicines.append(medicine)
+
+# Close the workbook
+workbook.close()
+
+# Commit the session to persist changes to the database
 db.session.commit()
 
 # Print a message to confirm successful addition to the database
